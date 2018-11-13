b53d25761302e895c607c6bd2c649761805c7528
Cassie Eggen <cleggen@uh.edu>
2018-11-13 15:37:34 -0600

    adding TODO messages to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_225743) do
+ActiveRecord::Schema.define(version: 2018_11_13_203419) do
 
   create_table "cars", force: :cascade do |t|
     t.string "vin"
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 2018_11_11_225743) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
