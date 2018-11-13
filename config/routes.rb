a1d8d4b743b8ef98bad88edf3bed785fce9dab3d
jaceflournoy <jacehflournoy@gmail.com>
2018-11-13 14:51:47 -0600

    - Added header - Added user login/logout functionality - Added password management - Added Session Management

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root 'cars#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  get 'home/index'
+  root 'home#index'
   resources :quotes
   resources :loans
   resources :employees
