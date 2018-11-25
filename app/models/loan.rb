860d540716086daa164160205929a061df9f3709
jaceflournoy <jacehflournoy@gmail.com>
2018-11-25 15:12:59 -0600

    -Fixed Loan Problem -Edited controllers/models/views to reflect changes

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,5 @@
 class Loan < ApplicationRecord
   #TODO add math here for loan by inputted interest rate
+  #
+  YEARS = %i[3 4 5]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_013649) do
+ActiveRecord::Schema.define(version: 2018_11_25_210547) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,10 +61,11 @@ ActiveRecord::Schema.define(version: 2018_11_21_013649) do
   end
 
   create_table "loans", force: :cascade do |t|
-    t.integer "type"
-    t.decimal "interestrate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "loanname"
+    t.integer "year"
+    t.decimal "interestrate"
   end
 
   create_table "quotes", force: :cascade do |t|
