757e665d5bcfdb0a05b1bfebb4f2ecdfebc80a4d
Cassie Eggen <cleggen@uh.edu>
2018-11-27 13:49:56 -0600

    Updated the view on Quote Form (adding a new Quote).

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -1,37 +1,45 @@
 var updateWholesalePrice;
+var updateSubTotalPrice;
 var updateTotalPrice;
 
 var wholesalePrice;
-var markupPrice;
+var subtotalPrice;
 var totalPrice;
 
 updateWholesalePrice = function() {
     let selection_id;
-    console.log('Got it');
     selection_id = $('#quote_car_id').val();
-    console.log(selection_id);
     return $.getJSON('/cars/' + selection_id + '/wholesale_price', {}, function(json, response) {
-        console.log("we've gotten this far");
-        return $('#wholesaleprice').text(json['wholesaleprice']);
+        wholesalePrice = parseFloat(json['wholesaleprice']);
+        return $('#wholesaleprice').val(wholesalePrice.toFixed(2));
+    });
+};
+
+updateSubTotalPrice = function() {
+    let selection_id;
+    selection_id = $('#quote_car_id').val();
+    return $.getJSON('/cars/' + selection_id + '/wholesale_price', {}, function(json, response) {
+        wholesalePrice = parseFloat(json['wholesaleprice']);
+        subtotalPrice = (wholesalePrice * 0.082) + wholesalePrice;
+        return $('#subtotalprice').text(subtotalPrice.toFixed(2));
     });
 };
 
 updateTotalPrice = function() {
     let selection_id;
-    console.log('Got it 2');
     selection_id = $('#quote_car_id').val();
-    console.log(selection_id);
     return $.getJSON('/cars/' + selection_id + '/wholesale_price', {}, function(json, response) {
-        console.log("round 2");
         wholesalePrice = parseFloat(json['wholesaleprice']);
-        markupPrice = (wholesalePrice * 0.082) + wholesalePrice;
-        return $('#totalprice').text(markupPrice);
+        subtotalPrice = (wholesalePrice * 0.082) + wholesalePrice;
+        totalPrice = subtotalPrice + (subtotalPrice * 0.043);
+        return $('#totalprice').val(totalPrice.toFixed(2));
     });
 };
 
 $(document).on('turbolinks:load', function() {
     console.log('cars loading');
 
+    $('#quote_car_id').on('change', updateSubTotalPrice);
     $('#quote_car_id').on('change', updateWholesalePrice);
     $('#quote_car_id').on('change', updateTotalPrice);
     $('#quote_car_id').trigger("change");
