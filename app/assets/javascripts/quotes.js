f67af479a86c1d801927fc1bffec880111a2ab91
Cassie Eggen <cleggen@uh.edu>
2018-12-01 20:39:44 -0600

    Finalized Quote math on Show page and Form page. Added Amortization table on finished quote Show page.

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -2,10 +2,6 @@ var updateWholesalePrice;
 var updateSubTotalPrice;
 var updateTotalPrice;
 
-var wholesalePrice;
-var subtotalPrice;
-var totalPrice;
-
 updateTotalPrice = function() {
     car_id = $('#quote_car_id').val();
     loan_id = $('#quote_loan_id').val();
@@ -38,10 +34,100 @@ updateWholesalePrice = function() {
 $(document).on('turbolinks:load', function() {
     console.log('cars loading');
 
-
     $('#quote_car_id').on('change', updateTotalPrice);
     $('#quote_loan_id').on('change', updateTotalPrice);
     $('#quote_car_id').on('change', updateSubTotalPrice);
     $('#quote_car_id').on('change', updateWholesalePrice);
     $('#quote_car_id').trigger("change");
 });
+
+
+/*DUSTINS SCRIPT*/
+
+function roundNumber(num, scale) {
+    if(!("" + num).includes("e")) {
+        return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+    } else {
+        var arr = ("" + num).split("e");
+        var sig = ""
+        if(+arr[1] + scale > 0) {
+            sig = "+";
+        }
+        return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+    }
+}
+
+function e(b,n){return Math.pow(b,n);}
+//compound 1 for yearly, 4 for quarterly, 12 for monthly, 24 for bimonthly
+
+function getPayment(principal,rate,years,compound) {
+    let r = rate / compound;
+    let n = years * compound;
+    document.getElementById("payment").innerText = roundNumber(principal*r/(1-e(1/(1+r),n)),2);
+}
+
+function getSchedule(period,principal,rate,years,compound){
+    let r = rate / compound;
+    let n = years * compound;
+    let payment = principal*r/(1-e(1/(1+r),n));
+    n = n - period + 1;
+    let begbal = payment*(1-e(1/(1+r),n))/r;
+    n--;
+    let endbal = payment*(1-e(1/(1+r),n))/r;
+    let paymentOnPrincipal = begbal - endbal;
+    return {
+        "begbal":roundNumber(begbal,2),
+        "interest":roundNumber(payment - paymentOnPrincipal,2),
+        "principal":roundNumber(paymentOnPrincipal,2),
+        "endbal":roundNumber(endbal,2)
+    }
+}
+
+function buildSchedule(principal, rate, years, compound) {
+  var body = document.getElementsByTagName('body')[0];
+  var tbl = document.createElement('table');
+  tbl.className = 'tbl';
+  tbl.setAttribute('border', '1');
+  var tbdy = document.createElement('tbody');
+  var hdr = document.createElement('tr');
+  var head = document.createElement('th');
+    head.innerText = "Year";
+    hdr.appendChild(head);
+    head = document.createElement('th');
+  head.innerText = "Beginning Balance";
+  hdr.appendChild(head);
+  head = document.createElement('th');
+  head.innerText = "Interest Payment";
+  hdr.appendChild(head);
+  head = document.createElement('th');
+  head.innerText = "Principal Payment";
+  hdr.appendChild(head);
+  head = document.createElement('th');
+  head.innerText = "Ending Balance";
+  hdr.appendChild(head);
+  tbdy.appendChild(hdr);
+
+  for (let i=1;i<years*compound+1;i++) {
+    let data = getSchedule(i,principal,rate,years,compound);
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    td.innerText = i.toString();
+    tr.appendChild(td);
+      td = document.createElement('td');
+      td.innerText = data.begbal.toLocaleString();
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = data.interest.toLocaleString();
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = data.principal.toLocaleString();
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = data.endbal.toLocaleString();
+    tr.appendChild(td);
+    tbdy.appendChild(tr);
+  }
+
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
+}
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -12,17 +12,23 @@ class Quote < ApplicationRecord
 #   TODO add math for price, constant multiplier and wholesale price
   def self.calc_final_price(car_id, loan_id)
     car = Car.find(car_id)
+      #wholesale price
     loan = Loan.find(loan_id)
+      #name, years, interest rate
 
     principle = car.wholesaleprice
     rate = loan.interestrate
     years = loan.year
 
-    markupPrice = principle + (principle * 0.082)
-    taxPrice = markupPrice + (markupPrice * 0.043)
-    interest = taxPrice * rate * years
+    r = rate / 4;
+    n = years * 4;
 
-    total_price = interest + taxPrice
+    markupPrice = principle * 1.082
+    taxPrice = markupPrice * 1.043
+
+    payment = taxPrice*r/(1-1/(1+r)**n)
+
+    total_price = payment * n
 
     return total_price
   end
